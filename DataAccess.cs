a268e9f4a5402ce3e0523b1c15985c01e8d23f5f
Daniel Berkowicz <daniel.berkowicz1@gmail.com>
2023-03-03 11:32:14 +0100

    Updated DB and gave DataAccess some new methods

--- DataAccess.cs
+++ DataAccess.cs
@@ -13,7 +13,7 @@ namespace miniprojectSQL
             {
                 try
                 {
-                    cnn.Execute($"INSERT INTO dbe_person(person_name) VALUES ('{name.ToLower()}')", new DynamicParameters());
+                    cnn.Execute($"INSERT INTO dabe_person(person_name) VALUES ('{name.ToLower()}')", new DynamicParameters());
                 }
                 catch (Npgsql.PostgresException e)
                 {
@@ -29,7 +29,7 @@ namespace miniprojectSQL
             {
                 try
                 {
-                    cnn.Execute($"INSERT INTO dbe_project (project_name) VALUES ('{projectName.ToLower()}')", new DynamicParameters());
+                    cnn.Execute($"INSERT INTO dabe_project (project_name) VALUES ('{projectName.ToLower()}')", new DynamicParameters());
                 }
                 catch (Npgsql.PostgresException e)
                 {
@@ -51,11 +51,11 @@ namespace miniprojectSQL
             {
                 try
                 {
-                    cnn.Execute(@$"INSERT INTO dbe_project_person (project_id, person_id, hours) VALUES ('{name_id}','{project_id}','{hour}')", new DynamicParameters());
+                    cnn.Execute(@$"INSERT INTO dabe_project_person (project_id, person_id, hours) VALUES ('{name_id}','{project_id}','{hour}')", new DynamicParameters());
                 }
                 catch (Npgsql.PostgresException e)
                 {
-                    //Console.WriteLine(e.MessageText);
+                    Console.WriteLine(e.MessageText);
                     return false;
                 }
                 return true;
@@ -66,7 +66,7 @@ namespace miniprojectSQL
         {
             using (IDbConnection cnn = new NpgsqlConnection(LoadConnectionString()))
             {
-                var output = cnn.Query<PersonModel>($"SELECT * FROM dbe_person", new DynamicParameters());
+                var output = cnn.Query<PersonModel>($"SELECT * FROM dabe_person", new DynamicParameters());
                 return output.ToList();
             }
         }
@@ -75,17 +75,45 @@ namespace miniprojectSQL
         {
             using (IDbConnection cnn = new NpgsqlConnection(LoadConnectionString()))
             {
-                var output = cnn.Query<ProjectModel>("SELECT * FROM dbe_project", new DynamicParameters());
+                var output = cnn.Query<ProjectModel>("SELECT * FROM dabe_project", new DynamicParameters());
                 return output.ToList();
             }
         }
+
+        internal static List<ProjectPersonModel> LoadProjectPerson()
+        {
+            using (IDbConnection cnn = new NpgsqlConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<ProjectPersonModel>($"SELECT * FROM dabe_project_person", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        internal static bool UpdateProjectPerson(int day, int hours)
+        {
+            using (IDbConnection cnn = new NpgsqlConnection(LoadConnectionString()))
+            {
+                try
+                {
+                    cnn.Query($"UPDATE dabe_project_person SET hours='{hours}' WHERE id='{day}'", new DynamicParameters());
+                }
+                catch (Npgsql.PostgresException e)
+                {
+                    Console.WriteLine(e.MessageText);
+                    return false;
+                }
+                return true;
+
+            }
+        }
+
         internal static bool ChangeUser(string oldName, string newName)
         {
             using (IDbConnection cnn = new NpgsqlConnection(LoadConnectionString()))
             {
                 try
                 {
-                    cnn.Query($"UPDATE dbe_person SET person_name='{newName.ToLower()}' WHERE person_name='{oldName.ToLower()}'", new DynamicParameters());
+                    cnn.Query($"UPDATE dabe_person SET person_name='{newName.ToLower()}' WHERE person_name='{oldName.ToLower()}'", new DynamicParameters());
                 }
                 catch (Npgsql.PostgresException e)
                 {
@@ -102,7 +130,7 @@ namespace miniprojectSQL
             {
                 try
                 {
-                    cnn.Query($"UPDATE dbe_project SET project_name='{newName.ToLower()}' WHERE project_name='{oldName.ToLower()}'", new DynamicParameters());
+                    cnn.Query($"UPDATE dabe_project SET project_name='{newName.ToLower()}' WHERE project_name='{oldName.ToLower()}'", new DynamicParameters());
                 }
                 catch (Npgsql.PostgresException e)
                 {
@@ -114,11 +142,29 @@ namespace miniprojectSQL
         }
 
         /* ////////////////////////TEST
+        /*
+        internal static bool TryLoadProjectPerson(int day)
+        {
+            using (IDbConnection cnn = new NpgsqlConnection(LoadConnectionString()))
+            {
+                try
+                {
+                    cnn.Query($"SELECT id FROM dabe_project_person WHERE id='{day}'", new DynamicParameters());
+                    return true;
+                }
+                catch (Npgsql.PostgresException e)
+                {
+                    Console.WriteLine(e.MessageText);
+                    return false;
+                }
+            }
+        }
+        
         internal static string TryLoadPerson(string person_name)
         {
             using (IDbConnection cnn = new NpgsqlConnection(LoadConnectionString()))
             {
-                var output = cnn.Query($"SELECT person_name FROM dbe_person WHERE person_name='{person_name.ToLower()}'", new DynamicParameters());
+                var output = cnn.Query($"SELECT person_name FROM dabe_person WHERE person_name='{person_name.ToLower()}'", new DynamicParameters());
                 return output.ToString();
             }
         }
@@ -129,7 +175,7 @@ namespace miniprojectSQL
             {
                 try
                 {
-                    cnn.Query($"SELECT * FROM dbe_project WHERE project_name='{project_name.ToLower()}'", new DynamicParameters());
+                    cnn.Query($"SELECT * FROM dabe_project WHERE project_name='{project_name.ToLower()}'", new DynamicParameters());
                 }
                 catch (Npgsql.PostgresException e)
                 {
@@ -143,7 +189,7 @@ namespace miniprojectSQL
         {
             using (IDbConnection cnn = new NpgsqlConnection(LoadConnectionString()))
             {
-                var output = cnn.Query<PersonModel>($"SELECT * FROM dbe_person WHERE person_name='{person_name.ToLower()}'", new DynamicParameters());
+                var output = cnn.Query<PersonModel>($"SELECT * FROM dabe_person WHERE person_name='{person_name.ToLower()}'", new DynamicParameters());
                 return output.ToList();
             }
         }
@@ -152,7 +198,7 @@ namespace miniprojectSQL
         {
             using (IDbConnection cnn = new NpgsqlConnection(LoadConnectionString()))
             {
-                var output = cnn.Query<ProjectModel>($"SELECT * FROM dbe_project WHERE project_name='{project_name.ToLower()}'", new DynamicParameters());
+                var output = cnn.Query<ProjectModel>($"SELECT * FROM dabe_project WHERE project_name='{project_name.ToLower()}'", new DynamicParameters());
                 return output.ToList();
             }
         }*/
@@ -164,7 +210,7 @@ namespace miniprojectSQL
             {
                 try
                 {
-                    cnn.Query($"UPDATE dbe_project SET project_name='{newName.ToLower()}' WHERE project_name='{oldName.ToLower()}'", new DynamicParameters());
+                    cnn.Query($"UPDATE dabe_project SET project_name='{newName.ToLower()}' WHERE project_name='{oldName.ToLower()}'", new DynamicParameters());
                 }
                 catch (Npgsql.PostgresException e)
                 {
--- database.sql
+++ database.sql
@@ -1,24 +1,26 @@
-ALTER TABLE "public"."dbe_project_person" DROP CONSTRAINT "FK_dbe_project_person_project_id";
-ALTER TABLE "public"."dbe_project_person" DROP CONSTRAINT "FK_dbe_person_project_person_id";
-DROP TABLE IF EXISTS "public"."dbe_project";
-DROP TABLE IF EXISTS "public"."dbe_person";
-DROP TABLE IF EXISTS "public"."dbe_project_person";
-CREATE TABLE "public"."dbe_project" ( 
-  "id" SERIAL,
-  "project_name" VARCHAR(50) NOT NULL,
-  CONSTRAINT "dbe_project_pkey" PRIMARY KEY ("id")
-);
-CREATE TABLE "public"."dbe_person" ( 
+ALTER TABLE "public"."dabe_project_person" DROP CONSTRAINT "FK_dabe_person_project_person_id";
+ALTER TABLE "public"."dabe_project_person" DROP CONSTRAINT "FK_dabe_project_person_project_id";
+DROP TABLE IF EXISTS "public"."dabe_person";
+DROP TABLE IF EXISTS "public"."dabe_project";
+DROP TABLE IF EXISTS "public"."dabe_project_person";
+CREATE TABLE "public"."dabe_person" ( 
   "id" SERIAL,
   "person_name" VARCHAR(25) NOT NULL,
-  CONSTRAINT "dbe_person_pkey" PRIMARY KEY ("id")
+  CONSTRAINT "dabe_person_pkey" PRIMARY KEY ("id"),
+  CONSTRAINT "UC_person_name" UNIQUE ("person_name")
 );
-CREATE TABLE "public"."dbe_project_person" ( 
+CREATE TABLE "public"."dabe_project" ( 
+  "id" SERIAL,
+  "project_name" VARCHAR(50) NOT NULL,
+  CONSTRAINT "dabe_project_pkey" PRIMARY KEY ("id"),
+  CONSTRAINT "UC_project_name" UNIQUE ("project_name")
+);
+CREATE TABLE "public"."dabe_project_person" ( 
   "id" SERIAL,
   "project_id" INTEGER NOT NULL,
   "person_id" INTEGER NOT NULL,
   "hours" INTEGER NOT NULL,
-  CONSTRAINT "dbe_project_person_pkey" PRIMARY KEY ("id")
+  CONSTRAINT "dabe_project_person_pkey" PRIMARY KEY ("id")
 );
-ALTER TABLE "public"."dbe_project_person" ADD CONSTRAINT "FK_dbe_project_person_project_id" FOREIGN KEY ("project_id") REFERENCES "public"."dbe_project" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
-ALTER TABLE "public"."dbe_project_person" ADD CONSTRAINT "FK_dbe_person_project_person_id" FOREIGN KEY ("person_id") REFERENCES "public"."dbe_person" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."dabe_project_person" ADD CONSTRAINT "FK_dabe_project_person_project_id" FOREIGN KEY ("project_id") REFERENCES "public"."dabe_project" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."dabe_project_person" ADD CONSTRAINT "FK_dabe_person_project_person_id" FOREIGN KEY ("person_id") REFERENCES "public"."dabe_person" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
